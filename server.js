497902623d90715af526498a265ecbb4b93d0026
Raqeeba-Yasin-PK <imcaptain004@gmail.com>
2025-12-12 23:49:10 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Web Server running inside Docker!");
+// Serve static HTML website
+app.use(express.static(path.join(__dirname, "public")));
+
+// API route
+app.get("/api", (req, res) => {
+  res.json({
+    message: "API is running inside Docker!",
+    timestamp: new Date()
+  });
 });
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
